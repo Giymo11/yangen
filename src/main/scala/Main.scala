03947543c7b0725008fac89fc5407fa5055fc8f6
Giymo11 <gizmo.head@yahoo.de>
2016-04-16 11:18:33 +0200

    major refactoring (there's still a little bug in there tho)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,5 @@
+import scala.collection.immutable.Range.Inclusive
 import scala.util.Try
-
 import scala.util.Random
 
 /**
@@ -8,16 +8,24 @@ import scala.util.Random
 object Main extends App {
   import ammonite.ops._
 
-  val generators = ls! cwd |? (_.ext == "gen") | (file => (file.name, rollTables(getTables(read(file).lines.toSeq)).mkString("\n")))
-  generators |! (tuple => write over(cwd / (tuple._1 + "-result.txt"), tuple._2))
+  val inputFiles = ls! cwd |? (_.ext == "gen")
 
-  // 1. Get tables to roll at
-  // 2. Roll in order, add new (optional) tables to roll at. Record / modify rolls
-  // 3. Print all results
+  val names = inputFiles | (_.name)
+  val results = inputFiles | read | (_.lines.toSeq) | getTables | rollTables
+
+  (names zip results) |! (tuple => write over(cwd / (tuple._1 + "-result.txt"), tuple._2))
 
   class Table(val name: String, val dice: Int, val map: Seq[Tuple2[Range, String]], val optional: Boolean, val distinct: Boolean) {
-    def get(roll: Int): Option[String] = map.find(_._1.contains(roll)).map(_._2)
-    val shortName = name.dropWhile(!_.isLetterOrDigit).takeWhile(_.isLetterOrDigit)
+
+    def roll(): Int =
+      Random.nextInt(dice) + 1
+
+    def get(roll: Int): Option[String] =
+      map.find(_._1.contains(roll)).map(_._2)
+
+    val shortName =
+      name.dropWhile(!_.isLetterOrDigit).takeWhile(_.isLetterOrDigit)
+
     def copy(
               name: String = this.name,
               dice: Int = this.dice,
@@ -27,33 +35,39 @@ object Main extends App {
   }
 
   object Table {
-    def apply(name: String, dice: Int, raw: Seq[String], optional: Boolean, distinct: Boolean): Table = {
-      val resultMap = raw.map(line => {
-        val roll = line.split(" ").head
-        val (min: Int, max: Int) = if(roll.contains("-")) { // format = "1-5:"
-        val min = roll.split("-").head.takeWhile(_.isDigit).toInt
-          val max = roll.split("-").drop(1).head.takeWhile(_.isDigit).toInt
-          (min, max)
-        } else {  // format = "3"
-          val res = roll.takeWhile(_.isDigit).toInt
-          (res, res)
-        }
-        (min to max, line.dropWhile(_ != ' ').dropWhile(_.isWhitespace))
-      })
-      new Table(name, dice, resultMap, optional, distinct)
-    }
+
+    def apply(name: String, dice: Int, raw: Seq[String], optional: Boolean, distinct: Boolean): Table =
+      new Table(name, dice, interpretRanges(raw), optional, distinct)
+
+    def interpretRanges(raw: Seq[String]): Seq[(Inclusive, String)] = raw.map(line => {
+      val range = line.split(" ").head
+      val answer = line.split(" ").tail.mkString(" ")
+
+      val (min: Int, max: Int) = if (range.contains("-")) {
+        // format = "1-5:"
+        val min = range.split("-").head.takeWhile(_.isDigit).toInt
+        val max = range.split("-").tail.head.takeWhile(_.isDigit).toInt
+        (min, max)
+      } else {
+        // format = "3:"
+        val res = range.takeWhile(_.isDigit).toInt
+        (res, res)
+      }
+
+      (min to max, answer)
+    })
   }
 
   def getTables(raw: Seq[String]): Seq[Table] = {
 
     def getTable(input: Seq[String]): Option[Table] = {
-      val table = input.takeWhile(line => !line.isEmpty)
+      val table = input.takeWhile(!_.isEmpty)
 
-      if(table(0).startsWith("--") && table(1).startsWith("Roll ")) {
-        val name = table(0).dropWhile(!_.isLetterOrDigit)
-        val dice = table(1).split(" ").drop(1).head.split("d").drop(1).head.takeWhile(_.isDigit).toInt
-        val optional = table(0).startsWith("----")
-        val distinct = table(0).takeWhile(_ == '-').length % 2 != 0
+      if(table.head.startsWith("--") && table.tail.head.startsWith("Roll ")) {
+        val name = table.head.dropWhile(!_.isLetterOrDigit)
+        val dice = table.tail.head.split(" ").tail.head.split("d").tail.head.takeWhile(_.isDigit).toInt
+        val optional = table.head.startsWith("----")
+        val distinct = table.head.takeWhile(_ == '-').length % 2 != 0
         Some(Table(name, dice, table.drop(2), optional, distinct))
       } else
         None
@@ -61,77 +75,59 @@ object Main extends App {
 
     val input = raw.dropWhile(!_.startsWith("--"))
 
-    if(input.isEmpty)
-      Seq()
-    else {
-      val firstTable: Option[Table] = getTable(input)
-      firstTable.toSeq ++ getTables(input.tail)
-    }
+    if(input.isEmpty) Seq()
+    else getTable(input).toSeq ++ getTables(input.tail)
   }
 
-  def rollTables(tables: Seq[Table]) = {
+  def rollTables(tables: Seq[Table]): Seq[String] = {
     val mandatory = tables.filter(!_.optional)
-    val rolls: Map[Table, Int] = mandatory.map(table => table -> (Random.nextInt(table.dice) + 1)).toMap
-
-    // find mandatory tables
-    // roll
-    // interpret results
-    // roll optionals / change results (IN ORDER)
+    val rolls: Map[Table, Int] = mandatory.map(table => table -> table.roll()).toMap
 
     var results: Seq[(Table, Int)] = Seq()
+
+    def addRoll(table: Table, roll: Int): Unit = {
+      results = results :+ (table -> roll)
+      if (!table.distinct) table.get(roll).foreach(interpretLine)
+    }
+
+    def deleteResult(table: Table) =
+      results = results.filterNot(_._1.shortName == table.shortName)
 
     def interpretLine(line: String): Unit = {
       val parsed = line.split("##").zipWithIndex
       val extras = parsed.filter(_._2 % 2 != 0).map(_._1.trim) // extracts everything like "##x y##
-      val normals = parsed.filter(_._2 % 2 == 0).map(_._1.trim)
-
-      //println("interpreting " + line)
 
       extras.foreach(extra => {
         val segments = extra.split(" ")
         val tableName = segments(0)
         val optTable = tables.find(_.shortName.equalsIgnoreCase(tableName))
 
+        def clamp(value: Int, min: Int, max: Int) = Math.max(min, Math.min(max, value))
+
         optTable.foreach(newTable =>
           if (segments.length > 1)
-            segments(1) match {
+            segments.tail.head match {
               case change if change.startsWith("+") || change.startsWith("-") =>
                 results = results.map(tuple =>
-                  if (tuple._1 == newTable) {
-                    val newRoll = tuple._2 + change.toInt
-                    newTable -> Math.max(1, Math.min(newTable.dice, newRoll))
-                  }
+                  if (tuple._1 == newTable) newTable -> clamp(tuple._2 + change.toInt, 1, newTable.dice)
                   else tuple
                 )
               case action if action.equalsIgnoreCase("roll") =>
-                if (newTable.distinct) results = results.filterNot(_._1.shortName == newTable.shortName)
-                val newRoll = Random.nextInt(newTable.dice) + 1
-                results = results :+ (newTable/*.copy(optional = true)*/ -> newRoll)
-                if (!newTable.distinct) newTable.get(newRoll).foreach(interpretLine)
+                if (newTable.distinct) deleteResult(newTable)
+                addRoll(newTable, newTable.roll())
               case amount if Try(amount.toInt).isSuccess && amount.toInt == 0 =>
-                results = results.filterNot(_._1.shortName == newTable.shortName)
+                deleteResult(newTable)
               case amount if Try(amount.toInt).isSuccess =>
-                if (newTable.distinct) results = results.filterNot(_._1.shortName == newTable.shortName)
-                val newRoll = amount.toInt
-                results = results :+ (newTable -> newRoll)
-                if (!newTable.distinct) newTable.get(newRoll).foreach(interpretLine)
+                deleteResult(newTable)
+                addRoll(newTable, amount.toInt)
             }
-          else println("Invalid extra syntax!")
-          //rolls = rolls.updated(newTable, Random.nextInt(newTable.dice) + 1)
+          else println("Invalid syntax!")
         )
       })
     }
 
-    for(table <- mandatory) {
-      rolls.get(table).foreach(roll => {
-        results = results :+ (table -> roll)
-
-        rolls.get(table).flatMap(x => table.get(x)) match {
-          case Some(s) => interpretLine(s)
-          case None => println(s"No roll in ${table.shortName} for ${rolls.get(table)}")
-        }
-      })
-    }
+    for(table <- mandatory)
+      rolls.get(table).foreach(roll => addRoll(table, roll))
 
     val lines = for{
       roll <- results
@@ -141,10 +137,6 @@ object Main extends App {
       header = if(!table.optional) table.name else ""
     } yield header + (if(!header.isEmpty && !parsed.isEmpty) "\n" else "") + "\t" + parsed
 
-    val cleaned = lines.filter(_.length > 3)
-
-    //println(cleaned.zip(cleaned.map(_.length)))
-
-    cleaned
+    lines.filter(_.length > 1)
   }
 }
